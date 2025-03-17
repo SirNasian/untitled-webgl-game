e24d41f6f5a71925a4c7553bf18a85dbca585dcd
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-17 19:02:17 +0800

    Set up initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const generateBundle = ({ entry, target, output_filename, output_directory }) => ({
+	entry,
+	target,
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			}
+		],
+	},
+	resolve: { extensions: [".ts", ".js"] },
+	output: { filename: output_filename, path: output_directory },
+})
+
+module.exports = [
+	generateBundle({
+		entry: path.join(__dirname, "src", "client.ts"),
+		target: "web",
+		output_filename: "client.js",
+		output_directory: path.join(__dirname, "public", "js"),
+	}),
+	generateBundle({
+		entry: path.join(__dirname, "src", "server.ts"),
+		target: "node",
+		output_filename: "server.js",
+		output_directory: __dirname,
+	}),
+];
