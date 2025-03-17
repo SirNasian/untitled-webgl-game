6e45037ae77e5bcff51d32e4ed54f7fea14512d5
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-17 23:25:26 +0800

    Set absolute upper limit of `MAX_CLIENTS` to 256
    This limitation is in place as client ids are transmitted in a single byte.

--- src/server/environment-variables.ts
+++ src/server/environment-variables.ts
@@ -1,4 +1,4 @@
 export default {
 	PORT: Number(process.env.PORT ?? 3000),
-	MAX_CLIENTS: Number(process.env.MAX_CLIENTS ?? 256),
+	MAX_CLIENTS: Math.min(Number(process.env.MAX_CLIENTS ?? 256), 256),
 };
