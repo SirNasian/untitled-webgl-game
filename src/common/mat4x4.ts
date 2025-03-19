ea13003263b47bfe6325902f69ae32c7c14aad5f
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-19 23:56:22 +0800

    Fix issue where depth is set to 0 during translation

--- src/common/mat4x4.ts
+++ src/common/mat4x4.ts
@@ -13,7 +13,7 @@ export const translate = (matrix: Mat4, x: number, y: number, z: number): Mat4 =
 		new Float32Array([
 			1, 0, 0, x,
 			0, 1, 0, y,
-			0, 0, 0, z,
+			0, 0, 1, z,
 			0, 0, 0, 1,
 		]) as Mat4,
 	);
