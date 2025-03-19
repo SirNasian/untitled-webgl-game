b5c4ba188f04fd0d48208146a15452a869416159
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-19 21:48:44 +0800

    Define explicit types to differentiate between Vec3 and Mat4

--- src/common/mat4x4.ts
+++ src/common/mat4x4.ts
@@ -1,27 +1,29 @@
+export type Mat4 = Float32Array & { readonly __type: "Mat4" };
+
 export const identity = () => new Float32Array([
 	1, 0, 0, 0,
 	0, 1, 0, 0,
 	0, 0, 1, 0,
 	0, 0, 0, 1,
-]);
+]) as Mat4;
 
-export const translate = (matrix: Float32Array, x: number, y: number, z: number): Float32Array =>
+export const translate = (matrix: Mat4, x: number, y: number, z: number): Mat4 =>
 	new Float32Array([
 		matrix[0],  matrix[1],  matrix[2],  matrix[3]  + x,
 		matrix[4],  matrix[5],  matrix[6],  matrix[7]  + y,
 		matrix[8],  matrix[9],  matrix[10], matrix[11] + z,
 		matrix[12], matrix[13], matrix[14], matrix[15],
-	]);
+	]) as Mat4;
 
-export const scale = (matrix: Float32Array, x: number, y: number, z: number): Float32Array =>
+export const scale = (matrix: Mat4, x: number, y: number, z: number): Mat4 =>
 	new Float32Array([
-		matrix[0] * x,  matrix[1],      matrix[2],      matrix[3],
-		matrix[4],      matrix[5] * y,  matrix[6],      matrix[7],
-		matrix[8],      matrix[9],      matrix[10] * z, matrix[11],
-		matrix[12],     matrix[13],     matrix[14],     matrix[15],
-	]);
+		matrix[0] * x, matrix[1],     matrix[2],      matrix[3],
+		matrix[4],     matrix[5] * y, matrix[6],      matrix[7],
+		matrix[8],     matrix[9],     matrix[10] * z, matrix[11],
+		matrix[12],    matrix[13],    matrix[14],     matrix[15],
+	]) as Mat4;
 
-export const rotateX = (matrix: Float32Array, angle: number): Float32Array => {
+export const rotateX = (matrix: Mat4, angle: number): Mat4 => {
 	const c = Math.cos(angle);
 	const s = Math.sin(angle);
 	return multiply(
@@ -30,12 +32,12 @@ export const rotateX = (matrix: Float32Array, angle: number): Float32Array => {
 			0, c, -s, 0,
 			0, s,  c, 0,
 			0, 0,  0, 1,
-		]),
-		matrix
+		]) as Mat4,
+		matrix,
 	);
 };
 
-export const rotateY = (matrix: Float32Array, angle: number): Float32Array => {
+export const rotateY = (matrix: Mat4, angle: number): Mat4 => {
 	const c = Math.cos(angle);
 	const s = Math.sin(angle);
 	return multiply(
@@ -44,12 +46,12 @@ export const rotateY = (matrix: Float32Array, angle: number): Float32Array => {
 			0,  1, 0, 0,
 			-s, 0, c, 0,
 			0,  0, 0, 1,
-		]),
-		matrix
+		]) as Mat4,
+		matrix,
 	);
 };
 
-export const rotateZ = (matrix: Float32Array, angle: number): Float32Array => {
+export const rotateZ = (matrix: Mat4, angle: number): Mat4 => {
 	const c = Math.cos(angle);
 	const s = Math.sin(angle);
 	return multiply(
@@ -58,13 +60,13 @@ export const rotateZ = (matrix: Float32Array, angle: number): Float32Array => {
 			s,  c, 0, 0,
 			0,  0, 1, 0,
 			0,  0, 0, 1,
-		]),
-		matrix
+		]) as Mat4,
+		matrix,
 	);
 };
 
-export const multiply = (a: Float32Array, b: Float32Array): Float32Array => {
-	const c = new Float32Array(16);
+export const multiply = (a: Mat4, b: Mat4): Mat4 => {
+	const c = new Float32Array(16) as Mat4;
 
 	for (let i = 0; i < a.length; i++) {
 		const row = Math.trunc(i / 4);
--- src/common/vec3.ts
+++ src/common/vec3.ts
@@ -1,16 +1,21 @@
-export const cross = (a: Float32Array, b: Float32Array): Float32Array =>
-	new Float32Array([
+export type Vec3 = Float32Array & { readonly __type: "Vec3" };
+
+export const create = (x: number, y: number, z: number): Vec3 =>
+	new Float32Array([x, y, z]) as Vec3;
+
+export const cross = (a: Vec3, b: Vec3): Vec3 =>
+	create(
 		a[1] * b[2] - a[2] * b[1],
 		a[0] * b[2] - a[2] * b[0],
 		a[0] * b[1] - a[1] * b[0],
-	]);
+	);
 
-export const dot = (a: Float32Array, b: Float32Array): number =>
+export const dot = (a: Vec3, b: Vec3): number =>
 	a[0] * b[0] + a[1] * b[1] + a[2] + b[2];
 
-export const normalize = (a: Float32Array): Float32Array => {
+export const normalize = (a: Vec3): Vec3 => {
 	const length = Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
-	return length > 0.0001
-		? new Float32Array([a[0] / length, a[1] / length, a[2] / length])
-		: new Float32Array([0, 0, 0]);
+	return (length > 0.0001)
+		? create(a[0]/length, a[1]/length, a[2]/length)
+		: create(0, 0, 0);
 };
